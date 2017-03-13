e7311b1fc7237313468da79dca9d50f1c1ecb37b
WildcherryBazzaire <Eieghtbitdolphin@gmail.com>
2017-03-13 08:37:12 -1000

    working code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("sanity Check");
+console.log("Sanity Check");
+
+//console.log(document.getElementById('use-id-method'));
+
+//finding html elements
+var idElement = document.getElementById('use-id-method');
+console.log(idElement);
+
+var tagElement = document.getElementsByTagName('script');
+console.log(tagElement);
+
+var classElement = document.getElementsByClassName('use-class-method');
+console.log(classElement[0]);
+console.log(classElement[1]);
