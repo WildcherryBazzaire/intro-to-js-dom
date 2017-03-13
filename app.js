e2fd891c8e98334ba065c1218a028a4edd277ffc
WildcherryBazzaire <Eieghtbitdolphin@gmail.com>
2017-03-13 08:48:13 -1000

    push

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ console.log(tagElement);
 var classElement = document.getElementsByClassName('use-class-method');
 console.log(classElement[0]);
 console.log(classElement[1]);
+
+//changing html Elements
+
+console.log('got the id value ' + idElement.innerHTML);
+idElement.innerHTML = 'I have been changed';
+
+console.log('got the Class Value ' + classElement[1].innerHTML);
+classElement[1].innerHTML = 'I have been changed by and array target //2';
